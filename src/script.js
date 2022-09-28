e058cd1879bd175e0b9e06a5b9e5dd54d0a0601b
Dan Rees <danrees8743@gmail.com>
2022-09-28 14:39:22 +0100

    105 starter files

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// DON'T EDIT THOSE LINES
+const dataURLBase = 'https://docs.google.com/spreadsheets/d/';
+const dataURLEnd = '/gviz/tq?tqx=out:json&tq&gid=';
+const id = '1C1-em4w0yHmd2N7__9cCSFzxBEf_8r74hQJBsR6qWnE';
+const gids = ['0', '1574569648', '1605451198'];
+// END OF DATA SETUP
+
+// TODO your code here
