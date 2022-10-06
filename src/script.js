53677094d0fbf337abebb545f7de8b8a3e000498
Niklas Lindenbaum <niklas.lindenbaum@t-online.de>
2022-10-06 15:23:45 +0200

    final result, working

--- src/script.js
+++ src/script.js
@@ -5,38 +5,71 @@ const id = "1C1-em4w0yHmd2N7__9cCSFzxBEf_8r74hQJBsR6qWnE";
 const gids = ["0", "1574569648", "1605451198"];
 // END OF DATA SETUP
 
-const employesUrl = fetch(
-  "https://docs.google.com/spreadsheets/d/1C1-em4w0yHmd2N7__9cCSFzxBEf_8r74hQJBsR6qWnE/gviz/tq?tqx=out:json&tq&gid=0"
-);
-const hiringUrl = fetch(
-  "https://docs.google.com/spreadsheets/d/1C1-em4w0yHmd2N7__9cCSFzxBEf_8r74hQJBsR6qWnE/gviz/tq?tqx=out:json&tq&gid=1574569648"
-);
-const salaryUrl = fetch(
-  "https://docs.google.com/spreadsheets/d/1C1-em4w0yHmd2N7__9cCSFzxBEf_8r74hQJBsR6qWnE/gviz/tq?tqx=out:json&tq&gid=1605451198"
-);
+const employesUrl = fetch(`${dataURLBase}${id}${dataURLEnd}${gids[0]}`);
+const hiringUrl = fetch(`${dataURLBase}${id}${dataURLEnd}${gids[1]}`);
+const salaryUrl = fetch(`${dataURLBase}${id}${dataURLEnd}${gids[2]}`);
 // TODO your code here
 
 const awaitFunc = async function () {
   const responses = await Promise.all([employesUrl, hiringUrl, salaryUrl]);
-  const json = responses.map(async (response) => {
-    const text = await response.text();
-    const object = await JSON.parse(text.substring(47).slice(0, -2));
-    const table = object.table;
-    console.log(table);
+  const datas = await Promise.all(
+    responses.map(async (response) => {
+      const text = await response.text();
+      return await JSON.parse(text.substring(47).slice(0, -2)).table.rows;
+    })
+  );
 
-    $(function () {
-      $("#employees").bootstrapTable({
-        url: "data1.json",
-        data: table.row,
-      });
+  datas[0].shift();
+  console.log(datas[0]);
+  const formattedData = [];
+  datas[0].forEach((item, index) => {
+    formattedData.push({
+      first: datas[0][index].c[0].v,
+      last: datas[0][index].c[1].v,
+      date: new Date(datas[1][index].c[0].f).toString().slice(4, 15),
+      salary: new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(datas[2][index].c[0].v),
     });
   });
-
-  datas.forEach((data) => data.parseInt());
-
-  const date = new Date();
-
-  // const data = await Promise.all(datas);
+  return formattedData;
 };
 
-awaitFunc();
+awaitFunc().then((formattedData) => {
+  $("#employees").bootstrapTable({
+    columns: [
+      {
+        title: "Last",
+        field: "last",
+        sortable: true,
+      },
+      {
+        title: "First",
+        field: "first",
+        sortable: true,
+      },
+      {
+        title: "Date",
+        field: "date",
+        sortable: true,
+      },
+      {
+        title: "Money",
+        field: "salary",
+        sortable: true,
+        sorter(a, b) {
+          a.replace(/[^\d\.]/g, "");
+          return (
+            Number(a.replace(/[^\d\.]/g, "")) -
+            Number(b.replace(/[^\d\.]/g, ""))
+          );
+        },
+      },
+    ],
+    sortable: true,
+    data: formattedData,
+  });
+});
+
+/*sort(a, b) => return new Date(a) - new Date(b)*/
